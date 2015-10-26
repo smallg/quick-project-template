48c0e2ef9c2754349ad536a662b01d97408a2144
smallg <gwpxjb@163.com>
2015-10-26 10:58:19 +0800

    fix express route conflict with angular route.

---
+++ app/scripts/angular/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by wpguo on 10/23/2015.
+ */
+'use strict';
+var app = angular.module('app', ['ngRoute','controllers']);
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {templateUrl: '/views/tpl/welcome.html', controller: 'WelcomeCtrl'})
+        .when('/test1',{templateUrl: '/views/tpl/test1.html', controller:'Test1Ctrl'})
+        .when('/test2',{templateUrl: '/views/tpl/test2.html', controller:'Test2Ctrl'})
+        .otherwise({redirectTo: '/'});
+
+    $locationProvider.html5Mode({enabled: true, requireBase: false});
+}]);
